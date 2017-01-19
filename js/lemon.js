c6bbdef29f89fc7c7e67f824a1632c227ef75257
Arturas Petkevicius <arturaspetkevicius@Arturass-MacBook-Pro.local>
2017-01-19 18:43:50 -0500

    Batch 1

--- js/lemon.js
+++ js/lemon.js
@@ -7,3 +7,29 @@ $(function(){
 	$('#Container').mixItUp();
 
 });
+
+
+var quoteData = [
+ 
+        {
+            category:"creativity",
+            quote:"Quote Test 123",
+            link:"www.google.com",
+            linkText: "Some Text - Goes Here"
+        }
+
+    ];
+
+    var container = $("#quote-container")
+ 
+    $.each(quoteData, function(key,data){
+        var quote ="";
+        quote = $(".quote.template").clone();
+        $(quote).removeClass("template");
+        $(quote).addClass(data.category);
+        $(quote).find(".quote-text").html(data.quote);
+        $(quote).find(".quote-credit").attr("href",data.link);
+        $(quote).find(".quote-credit").html(data.linkText);
+ 
+        $(quote).prependTo($(container));
+    });
