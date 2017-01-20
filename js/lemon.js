04aa831394b932bd85292c9ad7134eb4a31b8da6
Arturas Petkevicius <arturaspetkevicius@Arturass-MacBook-Pro.local>
2017-01-19 20:22:46 -0500

    last update / date

--- js/lemon.js
+++ js/lemon.js
@@ -249,3 +249,19 @@ var quoteData = [
  
         $(quote).prependTo($(container));
     });
+
+
+var d = new Date();
+
+var month = d.getMonth()+1;
+var day = d.getDate();
+
+var output = d.getFullYear() + '/' +
+    ((''+month).length<2 ? '0' : '') + month + '/' +
+    ((''+day).length<2 ? '0' : '') + day;
+
+
+$(".date").html(output);
+
+
+
